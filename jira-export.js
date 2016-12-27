d2aa80abefa180dd7eb153f31998d694c892b826
Christian Kaltepoth <christian@kaltepoth.de>
2016-12-27 16:19:14 +0100

    Initial version

---
+++ jira-export.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+const argv = require( "yargs" ).argv;
+const JiraClient = require( "./lib/jira-client" );
+const IssueConverter = require( "./lib/converter" );
+const utils = require( "./lib/utils" );
+
+let config = null;
+if( argv.config ) {
+  config = require( "./" + argv.config );
+}
+if( !config ) {
+  throw new Error( "Cannot open config" );
+}
+
+let output = argv.output;
+if( !output ) {
+  throw new Error( "You must specify an output file with --output" );
+}
+
+const converter = new IssueConverter( config );
+const client = new JiraClient( config.jira.url );
+
+console.log( "Validating JIRA API URL..." );
+
+client.getServerInfo().then( serverInfo => {
+
+  console.log( `Connected to: ${serverInfo}` );
+  return client.getIssueList( config.jira.project );
+
+} )
+.then( issues => {
+
+  console.log( `Found ${issues.length} issues for project ${config.jira.project}` );
+
+  const tasks = issues.map( issue => {
+    console.log( "Fetching details of: " + issue );
+    return client.getIssueDetails( issue ).then( details => {
+      return converter.convert( details );
+    } )
+  } );
+
+  return utils.runSequentially( tasks );
+
+} )
+.then( issues => {
+  const content = JSON.stringify( issues, null, 2 );
+  return utils.writeFile( output, content );
+} )
+.then( () => {
+  console.log( "DONE!" );
+} )
+.catch( error => {
+  console.log( error );
+} );
